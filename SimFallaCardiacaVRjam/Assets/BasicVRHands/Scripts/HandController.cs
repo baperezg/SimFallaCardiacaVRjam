9d6bec6a8ca8532223dc9be54b39700c3dacbdf9
baperezg <byron.perez@ieee.org>
2022-04-27 19:12:43 -0500

    Agarrar objetos con el control
    Se agregó la función de agarrar objetos con el control Oculus Touch

--- SimFallaCardiacaVRjam/Assets/BasicVRHands/Scripts/HandController.cs
+++ SimFallaCardiacaVRjam/Assets/BasicVRHands/Scripts/HandController.cs
@@ -1,15 +1,44 @@
 ﻿using UnityEngine;
+using UnityEngine.XR;
 using System.Collections;
+using System.Collections.Generic;
+
 
 public class HandController : MonoBehaviour {
 
     private Animator animator;
+	private InputDevice targetDevice;
+	private List<InputDevice> devices;
 
 	void Start () {
         animator = GetComponent<Animator>();
+		devices = new List<InputDevice>();
+		InputDevices.GetDevices(devices);
+
+		foreach(var item in devices)
+        {
+			Debug.Log(item.name + item.characteristics);
+			
+        }
 	}
 	
 	void Update () {
-        //animator.SetBool("isGrabbing", Input.GetKey(KeyCode.F));
+		targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue);
+		//Debug.Log("Botón primario: "+primaryButtonValue);
+		if (primaryButtonValue == true)
+        {
+			animator.SetBool("isGrabbing", true);
+		}
+
+		targetDevice.TryGetFeatureValue(CommonUsages.gripButton, out bool gripButtonValue);
+		if (gripButtonValue == true)
+		{
+			animator.SetBool("isGrabbing", true);
+		}
+
+		
+
+
+		//animator.SetBool("isGrabbing", Input.GetKey(KeyCode.F));
 	}
 }
