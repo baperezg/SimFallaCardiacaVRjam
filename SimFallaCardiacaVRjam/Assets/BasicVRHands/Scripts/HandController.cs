bd14eb5eeec7a9a10f98e601ceb48db1d17625d2
baperezg <byron.perez@ieee.org>
2022-05-01 19:03:51 -0500

    Interacción completa
    Se agregó teletransportación, modelos de manos animadas e interacción general. Se mejoraron las luces.
    TODO: Integrar grafo

--- SimFallaCardiacaVRjam/Assets/BasicVRHands/Scripts/HandController.cs
+++ SimFallaCardiacaVRjam/Assets/BasicVRHands/Scripts/HandController.cs
@@ -13,36 +13,36 @@ public class HandController : MonoBehaviour {
 	void Start () {
         animator = GetComponent<Animator>();
 
-		List<InputDevice> devices = new List<InputDevice>();
-		InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
-		InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
+		//List<InputDevice> devices = new List<InputDevice>();
+		//InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
+		//InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
 
-		foreach (var item in devices)
-		{
-			Debug.Log(item.name + item.characteristics);
-		}
+		//foreach (var item in devices)
+		//{
+		//	Debug.Log(item.name + item.characteristics);
+		//}
 
-		if (devices.Count > 0)
-		{
-			targetDevice = devices[0];
-		}
+		//if (devices.Count > 0)
+		//{
+		//	targetDevice = devices[0];
+		//}
 	}
 	
 	void Update () {
-		if (targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue) && primaryButtonValue)
-			animator.SetBool("isGrabbing", true);
-		else
-			animator.SetBool("isGrabbing", false);
+		//if (targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue) && primaryButtonValue)
+		//	animator.SetBool("isGrabbing", true);
+		//else
+		//	animator.SetBool("isGrabbing", false);
 
-		if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue) && triggerValue > 0.1f)
-			animator.SetBool("isGrabbing", true);
-		else
-			animator.SetBool("isGrabbing", false);
+		//if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue) && triggerValue > 0.1f)
+		//	animator.SetBool("isGrabbing", true);
+		//else
+		//	animator.SetBool("isGrabbing", false);
 
-		if (targetDevice.TryGetFeatureValue(CommonUsages.gripButton, out bool gripButtonValue) && gripButtonValue)
-			animator.SetBool("isGrabbing", true);
-		else
-			animator.SetBool("isGrabbing", false);
+		//if (targetDevice.TryGetFeatureValue(CommonUsages.gripButton, out bool gripButtonValue) && gripButtonValue)
+		//	animator.SetBool("isGrabbing", true);
+		//else
+		//	animator.SetBool("isGrabbing", false);
 
 
 
--- SimFallaCardiacaVRjam/Assets/HandPresence.cs
+++ SimFallaCardiacaVRjam/Assets/HandPresence.cs
@@ -5,41 +5,99 @@ using UnityEngine.XR;
 
 public class HandPresence : MonoBehaviour
 {
+    public bool showController = false;
+    public InputDeviceCharacteristics controllerCharacteristics;
     public List<GameObject> controllerPrefabs;
+    public GameObject handModelPrefab;
+
     private InputDevice targetDevice;
     private GameObject spawnedController;
+    private GameObject spawnedHandModel;
+    private Animator handAnimator;
 
     // Start is called before the first frame update
     void Start()
     {
-        List<InputDevice> devices = new List<InputDevice>();       
-        InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
-        InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
+        TryInitialize();
+    }
+
+    // Check if controller is present after the initialization
+    void TryInitialize()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
 
         foreach (var item in devices)
         {
-            Debug.Log(item.name + item.characteristics);
+            Debug.Log(item.name + " Características: " + item.characteristics);
         }
 
-        if(devices.Count > 0)
+        if (devices.Count > 0)
         {
             targetDevice = devices[0];
             GameObject prefab = controllerPrefabs.Find(controller => controller.name == targetDevice.name);
             if (prefab)
             {
-                Instantiate(prefab, transform);
+                spawnedController = Instantiate(prefab, transform);
             }
             else
             {
-                Debug.LogError("Objeto no corresponde al controller model");
+                Debug.LogError("Did not find corresponding controller model");
                 spawnedController = Instantiate(controllerPrefabs[0], transform);
             }
+
+            spawnedHandModel = Instantiate(handModelPrefab, transform);
+            handAnimator = spawnedHandModel.GetComponent<Animator>();
+        }
+    }
+
+    // Upadate hand animation
+    void UpdateHandAnimation()
+    {
+        if(targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+        {
+            handAnimator.SetFloat("Trigger", triggerValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Trigger", 0);
+        }
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+        {
+            handAnimator.SetFloat("Grip", gripValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Grip", 0);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!targetDevice.isValid)
+        {
+            TryInitialize();
+        }
+        else
+        {
+            if (showController)
+            {
+                spawnedController.SetActive(true);
+                spawnedHandModel.SetActive(false);
+            }
+            else
+            {
+                spawnedController.SetActive(false);
+                spawnedHandModel.SetActive(true);
+                UpdateHandAnimation();
+            }
+        }       
+
+        //Mostrar actividad de los botones en la consola
+        /*
         if (targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue) && primaryButtonValue)
             Debug.Log("Presionando primary button");
         
@@ -48,8 +106,7 @@ public class HandPresence : MonoBehaviour
 
         if (targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue) && primary2DAxisValue != Vector2.zero)
             Debug.Log("Primary touchpad: " + primary2DAxisValue);
-           
-
+          */
 
     }
 }
